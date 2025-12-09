41b9d9c2a0aa606c6a54c1f903d4c090e313fa33
azamxoja2012 <azamxojaboxodirov@gmail.com>
2025-12-09 16:33:31 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+for (let i = 1; i<5; i++){
+    console.log("Шаг:",  i)
+}
+// i++ = i+1
+
+
+let fruits = [ "apple", "banana", "orange"   ];
+console.log(fruits)
+for (let i = 0; i  < fruits.length; i++) {
+    console.log(fruits[i]);
+}
+
+
+for ( let i = 0;  i  <= 10; )
